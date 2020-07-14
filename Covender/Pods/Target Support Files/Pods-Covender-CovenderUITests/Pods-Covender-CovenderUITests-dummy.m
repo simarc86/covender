2266a53bced3056bb200c76f4853f32a6d75635b
Marc Tamarit <itech@mercacerca.es>
2020-07-14 18:36:58 +0200

    setup enviroment

---
+++ Covender/Pods/Target Support Files/Pods-Covender-CovenderUITests/Pods-Covender-CovenderUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Covender_CovenderUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Covender_CovenderUITests
+@end
---
+++ Covender/Pods/Target Support Files/Pods-Covender/Pods-Covender-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Covender : NSObject
+@end
+@implementation PodsDummy_Pods_Covender
+@end
